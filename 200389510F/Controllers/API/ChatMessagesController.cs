24406ce6323c75d4c20df26d9a0676159a764e5d
Michael Roussel <pokemon_pidgeot@msn.com>
2019-04-15 16:35:36 -0400

    added wep API - not sure if the date is a field as can't test the actual database

---
+++ 200389510F/Controllers/API/ChatMessagesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using _200389510F.Models;
+
+namespace _200389510F.Controllers.API
+{
+    public class ChatMessagesController : ApiController
+    {
+        private ChatMessageContext db = new ChatMessageContext();
+
+        // GET: api/ChatMessages
+        public IQueryable<ChatMessage> GetChatMessages()
+        {
+            return db.ChatMessages;
+        }
+
+        // GET: api/ChatMessages/5
+        [ResponseType(typeof(ChatMessage))]
+        public IHttpActionResult GetChatMessage(int id)
+        {
+            ChatMessage chatMessage = db.ChatMessages.Find(id);
+            if (chatMessage == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(chatMessage);
+        }
+
+        // PUT: api/ChatMessages/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutChatMessage(int id, ChatMessage chatMessage)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != chatMessage.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(chatMessage).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ChatMessageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ChatMessages
+        [ResponseType(typeof(ChatMessage))]
+        public IHttpActionResult PostChatMessage(ChatMessage chatMessage)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.ChatMessages.Add(chatMessage);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = chatMessage.ID }, chatMessage);
+        }
+
+        // DELETE: api/ChatMessages/5
+        [ResponseType(typeof(ChatMessage))]
+        public IHttpActionResult DeleteChatMessage(int id)
+        {
+            ChatMessage chatMessage = db.ChatMessages.Find(id);
+            if (chatMessage == null)
+            {
+                return NotFound();
+            }
+
+            db.ChatMessages.Remove(chatMessage);
+            db.SaveChanges();
+
+            return Ok(chatMessage);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ChatMessageExists(int id)
+        {
+            return db.ChatMessages.Count(e => e.ID == id) > 0;
+        }
+    }
+}
--- 200389510F/Global.asax.cs
+++ 200389510F/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace _200389510F
 {
@@ -14,6 +15,12 @@ namespace _200389510F
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings
+                .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters
+                .Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+
             Database.SetInitializer(new ChatMessageContextInitializer());
 
             AreaRegistration.RegisterAllAreas();
