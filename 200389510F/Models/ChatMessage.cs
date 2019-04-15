9723565b0bd3124ce9cf99da809a4224eb06eabc
Michael Roussel <pokemon_pidgeot@msn.com>
2019-04-15 15:20:19 -0400

    added chat model and git repo link

---
+++ 200389510F/Models/ChatMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _200389510F.Models
+{
+    //GIT URL IS https://github.com/MichaelRoussel/Comp2084Final
+    public class ChatMessage
+    {
+
+        public virtual int ID { get; set; }
+        [Required]
+
+        public virtual string Message { get; set; }
+        public virtual string Name { get; set; }
+        public virtual DateTime Date { get; set; }
+
+
+
+    }
+}
