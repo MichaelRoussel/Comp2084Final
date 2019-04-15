5a8aeb8259680e7ea72b21a5ad5c0bc079d69074
Michael Roussel <pokemon_pidgeot@msn.com>
2019-04-15 16:37:00 -0400

    added message in ChatMessage model about database issue

--- 200389510F/Models/ChatMessage.cs
+++ 200389510F/Models/ChatMessage.cs
@@ -7,6 +7,8 @@ using System.Web;
 namespace _200389510F.Models
 {
     //GIT URL IS https://github.com/MichaelRoussel/Comp2084Final
+    //Visual studio was being a pain, could not find the database so I have the view working as it should, however I am not sure if 
+    //the seeding or creating of a post actually works, but it should as the code looks correct
     public class ChatMessage
     {
 
